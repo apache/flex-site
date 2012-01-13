848c4d32fa6a6989b96803230aedda58d4548ada
Peter Elst <peterelst@apache.org>
2012-01-13 14:08:33 +0000

    whitespace to trigger site build
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flex/site/trunk@1231078 13f79535-47bb-0310-9956-ffa450edef68

--- lib/view.pm
+++ lib/view.pm
@@ -196,4 +196,4 @@ sub templatesfolder {
            "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
            KIND, either express or implied.  See the License for the
            specific language governing permissions and limitations
-           under the License.
+           under the License. 
