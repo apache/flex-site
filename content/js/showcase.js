cf7e0d746553d912a95b994b52b029ff60889ced
Maurice Amsellem <mamsellem@apache.org>
2013-12-19 17:39:23 +0000

    IN PROGRESS - FLEX-33907 Showcase section in web site ajax implementation (test2)
    git-svn-id: https://svn.apache.org/repos/asf/flex/site/trunk@1552373 13f79535-47bb-0310-9956-ffa450edef68

--- content/js/showcase.js
+++ content/js/showcase.js
@@ -56,23 +56,6 @@
 
 }
 
-/*  List of showcase entries, in alphabetical order
-added when document is ready
- *  **/
-
-/*  Template for new entry:
- addShowcaseEntry({
- title: "",
- description: "" ,
- author: "",
- website: "",
- dev: "" ,
- links: [ "", "" ],
- imgDir: "",
- images: [ "" ]
- });
- */
-
   $(document).ready(function () {
 
       if (!document.getElementById("showcaseContainer"))
@@ -80,124 +63,65 @@ added when document is ready
 
     $("a.gallery").fancybox({ cyclic: false});
 
-     alert("showcase-ready");
+     alert("showcase-ready 3");
      // retrieve showcase XML file
    //   $.get("data/showcase.xml", "false", processShowcaseXML,"xml")   ;
 
-      $.ajax({ url: "data/showcase.xml", type: "GET", dataType: "xml",
-          sucess: processShowcaseXML,
+      $.ajax({ url: "showcase/data/showcase.xml", type: "GET", dataType: "xml",
+          complete: function (result, state) {
+              if (state == "success"){
+                  alert("complete" );
+                  processShowcaseXML(result.responseXML) ;
+              }
+          },
           error: function (result, state, error) {
-              alert("data/showcase.xml file is not reachable:" + error)
+              alert("http://localhost/BB_WEBSERVICES_5/showcase/showcase.xml file is not reachable:" + error)
           }
       });
 
-
-
-    /* add showcase entries when document is loaded */
-    addShowcaseEntry({
-        title: 'bCommunities -  Secure B2B Collaboration',
-        description: "A disruptively efficient online platform that enables businesses to powerfully "+
-        "and securely interconnect their operations with their clients and suppliers in minutes. "+
-         "Features silo-bursting project management, communications, CRM and microblogging while fostering best practices and accountability.",
-        author: "Dan Samper",
-        website: 'http://bcommunities.com',
-        imgDir: "bCommunities"  ,
-        images: [ "Communications Inbox.jpg" , "Directory - Company Level.jpg" , "Project Setup View.jpg","User Settings.jpg"]
-    });
-
-      addShowcaseEntry({
-          title: "Documobile - Electronic Parts Catalog Interface",
-          description: "Documobile allows users to view parts catalogs, technical documents, find technical information visually with our icon driven platform or search by keywords, descriptions, part numbers or other identifying information. " +
-              "With a few swipes of a finger, Documobile helps users quickly and easily access the exact document or part they want. ",
-          author: "Digabit, Inc.",
-          website: "http://www.digabit.com/",
-          links: [ "iTunes", "https://itunes.apple.com/us/app/documobile/id731514836" ],
-          imgDir: "Documobile",
-          images: [ "documobile1.png", "documobile2.png", "documobile3.png"]
-      });
-
-    addShowcaseEntry({
-        title: "Goal Manager",
-        description: "GOAL 2014 - Football Manager is a free football management game for mobile phones and tablets, now available for iOS and Android. " +
-            "In this game a growing community of more than 3000 daily active managers from all over Europe face each other in a multi-level League system and a general Cup competition. " +
-            "To achieve victory, the managers have to decide tactics and formations, buy and sell players, plan individual trainings, study their opponents, build up a stadium and manage finances and sponsorships.",
-        author: "GoalGames GmbH",
-        dev: "Apache Flex 4.10 - PureMVC - Native Extentions",
-        website: "http://www.goal-games.de/",
-        links: ["iTunes","https://itunes.apple.com/de/app/goal-manager/id575222774?mt=8&affId=1881396&ign-mpt=uo%3D4",
-            "Google Play", "https://play.google.com/store/apps/details?id=air.de.goalgames.GoalManagerCH&hl=de"] ,
-        imgDir: "Goal",
-        images: [ "Login.jpg" , "Stadion.jpg", "Taktik.jpg"]
-    });
-
-    addShowcaseEntry({
-        title: "My Dojo for Dota 2",
-        description: "My Dojo for Dota 2 is a mobile application that tracks personal statistics from the online video game Dota 2. " +
-            "It shows the results of matches in a easy to understand interface with extended information of each player. ",
-        author: "Jorge Yabra",
-        links: [ "iTunes", "https://itunes.apple.com/us/app/my-dojo-for-dota-2/id689886683?ls=1&mt=8"]   ,
-        imgDir: "Dojo",
-        images: [ "1.png", "2.png", "3.png"]
-    });
-
-  /*   TODO Online Designer Tool
-    addShowcaseEntry({
-        title: "Online Designer Tool",
-        description: "",
-        author: "",
-        website: "",
-        imgDir: "",
-        images: [  ]
-    });
-*/
-
-       addShowcaseEntry({
-       title: "ReDiLab Reaction Diffusion Laboratory",
-       description: "ReDiLab is my reaction diffusion (http://en.wikipedia.org/wiki/Reaction%E2%80%93diffusion_system) explorer written in Apache Flex that uses AGAL and Stage3D to run cellular automata simulating a variety of RD models. " ,
-       author: "Simon Gladman",
-       website: "http://flexmonkey.blogspot.co.uk/search/label/ReDiLab",
-       links: [ "Demo", "http://www.flexmonkey.co.uk/redilab-bin-release-005/ReactionDiffusionLab.html" ],
-       imgDir: "Redilab",
-       images: [ "Screen Shot 1.png", "Screen Shot 2.png", "Screen Shot 3.png"]
-       });
-
-      /*
-      addShowcaseEntry({
-          title: "6Play : digital platform for French TV channels (M6, W9 and 6ter)",
-          description: "Award winning Social TV platform for M6 and all the group's channels (M6, W9 and 6ter), including live TV ( from France only ), premium video features , Facebook and Twitter integration.<br>"
-       +"M6, french broadcast group, has launched a unique digital entrance platform to all the group's channels, and recently won a national 'Best Social TV application' price. " +
-          "This application is a perfect demonstration of skinning, animation and data management capabilities given by Apache Flex SDK. ",
-          author: "Erick Ghaumez",
-          dev: "Apache Flex 4.10",
-          links: [ "Live app", "http://www.6play.fr" ],
-          imgDir: "6play",
-          images: [ "6play.png", "6play-6ter.png", "6play-M6-programmes.png" ]
-      });
-        */
-
-      addShowcaseEntry({
-        title: 'TrafficLIVE AIR Application',
-        description: "TrafficLIVE is a comprehensive creative business management system that provides visibility into resources, work and finances all in one place. " +
-            "Creative businesses all over the globe are using TrafficLIVE to improve efficiency and increase profit. " ,
-        author: "Marcus Wilkinson",
-        website: 'http://trafficlive.com',
-        imgDir: "Traffic",
-        images: [ "TrafficLIVE-data-visualisation.png" ]
-    });
-
-    addShowcaseEntry({
-        title: "VeraType",
-        description: "VeraType converts images into text and text into images. It is also called ASCII Art but in this case you can write your own message and form into an image. ",
-        author: "Judah Frangipane",
-        website: "http://www.velara3.com/wp/2013/03/21/veratype-for-win-and-mac/ ",
-        imgDir: "VeraType",
-        images: [ "feature-graphic.png","screenshot01.png" ]
-    });
-
-
 });
 
-function processShowcaseXML( xmlData)  {
-       alert("XML="+xmlData);
+/**
+ *
+ * @param xmlData  == document
+ */
+function processShowcaseXML( xmlDoc)   {
+      var items = xmlDoc.getElementsByTagName("app");
+     var i;
+    var item;
+
+      for (i = 0; i < items.length; i++){
+          item = items[i];
+          // build links Array
+          var linksXML = item.getElementsByTagName("link") ;
+          var j;
+          var links = [];
+          for ( j=0; j < linksXML.length;j++){
+              links.push(linksXML[j].getAttribute("header")) ;
+              links.push(linksXML[j].getAttribute("url"));
+          }
+          // builds images array
+          var imagesRootXML = item.getElementsByTagName("images") ;
+          var images = [];
+          if (imagesRootXML.length > 0 ){
+              imagesRootXML = imagesRootXML[0] ;
+              var imgBaseDir = imagesRootXML.getAttribute("baseDir");
+              var imagesXML = imagesRootXML.getElementsByTagName("image");
+              for (j = 0; j < imagesXML.length; j++) {
+                  images.push(imagesXML[j].getAttribute("file"));
+              }
+          }
+
+          addShowcaseEntry({
+              title: item.getAttribute("title") ,
+              description: item.getElementsByTagName("description")[0].textContent,
+              author: item.getAttribute("author"),
+              website: item.getAttribute("website") ,
+              dev: item.getAttribute("dev"),
+              links: links,
+              imgDir: imgBaseDir,
+              images: images
+          });
+      }
 
 }
